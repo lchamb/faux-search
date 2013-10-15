1f02c446f4a0a84de1dbbef44e4057672f5ecb78
Luke Chamberlin <lukechamberlin@gmail.com>
2013-10-15 16:42:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FauxSearch::Application.config.secret_key_base = '5659b51485f05ee2bf89bca4ae0107e79f41e109c8f47904502345df6d03790dfeb1d25afa0e38832d394c296f79835c355cab26372e324b89546cef96422a87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FauxSearch::Application.config.session_store :cookie_store, key: '_faux_search_session'
