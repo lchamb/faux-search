53357b29467e0c9bf2490272db4b88e69411308b
Luke Chamberlin <lukechamberlin@gmail.com>
2013-10-17 17:57:19 -0400

    Add fake cats to controller and some views and partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+  
   def index
   end
+
+  def search_results
+    @products = [
+      { title: "Feline Cat", price: 50, url: "http://www.placekitten.com/240/240" },
+      { title: "Fluffy Cat", price: 65, url: "http://www.placekitten.com/230/230" },
+      { title: "Friendly Cat", price: 75, url: "http://www.placekitten.com/220/220" },
+      { title: "Bold Cat", price: 40, url: "http://www.placekitten.com/210/210" }
+    ]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ FauxSearch::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+
+  get '/search', to: 'home#search_results'
 end
