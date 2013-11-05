8f6380077b30d224bba4356f6b1acbac25b7ee4c
Luke Chamberlin <lukechamberlin@gmail.com>
2013-11-05 16:57:07 -0500

    show 2 pages of 12 cats per page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,20 @@ class HomeController < ApplicationController
 
   def search_results
     @query = params[:q]
-    @results = Kaminari.paginate_array(scope).page(params[:page]).per(4)
+    @results = Kaminari.paginate_array(scope).page(params[:page]).per(12)
   end
 
-
   private
+
   def scope
-    [
-      FauxResult.new({ title: "Feline Cat", price: 50, url: "http://www.placekitten.com/240/240" }),
-      FauxResult.new({ title: "Fierce Cat", price: 50, url: "http://www.placekitten.com/234/234" }),
-      FauxResult.new({ title: "Fluffy Cat", price: 65, url: "http://www.placekitten.com/230/230" }),
-      FauxResult.new({ title: "Naughty Cat", price: 65, url: "http://www.placekitten.com/224/224" }),
-      FauxResult.new({ title: "Friendly Cat", price: 75, url: "http://www.placekitten.com/220/220" }),
-      FauxResult.new({ title: "Silly Cat", price: 75, url: "http://www.placekitten.com/214/214" }),
-      FauxResult.new({ title: "Blasé Cat", price: 75, url: "http://www.placekitten.com/248/248" }),
-      FauxResult.new({ title: "Bold Cat", price: 40, url: "http://www.placekitten.com/200/200" })
-    ]
+    prefixes = %w(Feline Fierce Fluffy Naughty Friendly Silly Blasé Bold)
+
+    23.times.map do |index|
+      FauxResult.new({ 
+        title: "#{prefixes.sample} #{prefixes.sample} Cat", 
+        price: rand(140) + 10, 
+        url: "http://www.placekitten.com/#{200 + index}/#{200 + index}" 
+        })
+    end
   end
 end
